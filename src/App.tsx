1812249a75e2fa75cfb66e4dbeea5359f25c8bcd
Ömer <savas.qrx@gmail.com>
2025-12-16 23:56:31 +0100

    create quiz component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import Quiz from "./components/quiz";
+
 function App() {
-  const appName = "Quiz App";
   return (
     <>
-      <div className="text-2xl font-bold flex justify-center p-10 bg-gray-200 border shadow-2xl">
-        {appName}
+      <div className="text-2xl font-bold flex flex-col items-center justify-center p-10 bg-gray-200 border shadow-2xl">
+        {" "}
+        <h1>Quiz App</h1>
+        <div>
+          <Quiz />
+        </div>
       </div>
     </>
   );
