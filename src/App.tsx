7c83ca50570f1bf664230fd0e6e7abb1bcd9e661
Ömer <savas.qrx@gmail.com>
2025-12-17 15:01:50 +0100

    create quizzes and next prev buttons

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="mx-auto w-1/2 items-center">
-        <h1 className="font-bold text-3xl p-10 bg-gray-200 border shadow-2xl text-center">
+        <h1 className="font-bold text-3xl p-10 bg-gray-200 border  text-center">
           Quiz App
         </h1>
         <div>
--- src/components/quiz.tsx
+++ src/components/quiz.tsx
@@ -1,3 +1,5 @@
+import { useRef, useState, type SetStateAction } from "react";
+
 export default function Quiz() {
   type Question = {
     question: string;
@@ -26,22 +28,74 @@ export default function Quiz() {
       answer: "Harper Lee",
     },
   ];
+  const initialAnswers = [null, null, null];
+
+  const [userAnswers, setUserAnswers] = useState(initialAnswers);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  function handleSelectOption(option) {
+    const newAnswers = [...userAnswers];
+    newAnswers[currentQuestion] = option;
+
+    setUserAnswers(newAnswers);
+  }
+
+  function handleNextQuestion() {
+    if (currentQuestion < questionBank.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    }
+  }
+  function handlePrevQuestion() {
+    if (currentQuestion > 0) {
+      setCurrentQuestion(currentQuestion - 1);
+    }
+  }
   return (
-    <div>
-      {questionBank.map((q, index) => (
-        <div key={index} className="mb-6 p-4  rounded shadow">
-          <h2 className="font-semibold mb-2">{q.question}</h2>
-          <ul>
-            {q.options.map((option, idx) => (
-              <li key={idx} className="flex flex-col mb-2 ">
-                <button className="cursor-pointer px-4 py-2 bg-gray-300  text-black rounded hover:bg-gray-400">
-                  {option}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
+    <div className="flex flex-col gap-4 mx-auto justify-center p-4 border rounded shadow m-2">
+      <h2>Question {currentQuestion}</h2>
+      <p>{questionBank[currentQuestion].question}</p>
+      {questionBank[currentQuestion].options.map((option) => (
+        <button
+          className="p-2 bg-gray-300 rounded-sm"
+          onClick={() => handleSelectOption(option)}
+        >
+          {option}
+        </button>
       ))}
+      <div id="nav-buttons" className="flex  gap-2">
+        <button
+          className="p-2 bg-gray-300 rounded-sm"
+          onClick={handlePrevQuestion}
+        >
+          {"<"}
+        </button>
+        <button
+          className="p-2 bg-gray-300 rounded-sm"
+          onClick={handleNextQuestion}
+        >
+          {">"}
+        </button>
+      </div>
     </div>
+    // <div>
+    //   {questionBank.map((q, index) => (
+    //     <div key={index} className="mb-6 p-4  rounded shadow">
+    //       <h2 className="font-semibold mb-2">{q.question}</h2>
+    //       <ul>
+    //         {q.options.map((option, idx) => (
+    //           <li key={idx} className="flex flex-col mb-2 ">
+    //             <button
+    //               onClick={() => handleSelecOption(option)}
+    //               className="cursor-pointer px-4 py-2 bg-gray-300  text-black rounded hover:bg-gray-400"
+    //             >
+    //               {option}
+    //             </button>
+    //           </li>
+    //         ))}
+    //         <p>{selectedOption}</p>
+    //       </ul>
+    //     </div>
+    //   ))}
+    // </div>
   );
 }
