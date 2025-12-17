20c5d5b90f33d5718f295b781c62f2461dedbf97
Ömer <savas.qrx@gmail.com>
2025-12-17 17:51:21 +0100

    create results

---
+++ src/components/Results.tsx
@@ -0,0 +1,10 @@
+export default function Results() {
+  return (
+    <div>
+      <h2>Quiz completed"</h2>
+      <div>
+        <p>Your results will be displayed here.</p>
+      </div>
+    </div>
+  );
+}
